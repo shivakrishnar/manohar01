ef03e1099a2b8c56cad2c64d00b0997b551e2b46
shivakrishnar <shivakrishnar82@gmail.com>
2025-11-25 12:06:36 +0000

    Fix typo in ValidationValues class name

--- src/main/java/com/mediant/api/dataexchange/trigger/ValidationValues.java
+++ src/main/java/com/mediant/api/dataexchange/trigger/ValidationValues.java
@@ -1,8 +1,8 @@
 package com.mediant.api.dataexchange.trigger;
 
-public final class ValidationVlues {
+public final class ValidationValues {
     public static final int MAX_FILENAME_LENGTH = 255;
     public static final String ALLOWED_CHARS_REGEX = "[A-Za-z0-9_\\- .]+";
 
-    private ValidationVlues() {}
+    private ValidationValues() {}
 }
